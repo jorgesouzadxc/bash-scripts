26bf9540c66328b4590ffd2aa83e4e55f6c73ea8
Jorge Souza da Silva <jorge.souza-da-silva@dxc.com>
2018-12-24 21:16:55 -0200

    Criado declaracao-condicional.sh

---
+++ declaracao-condicional.sh
@@ -0,0 +1,19 @@
+main() {
+# Bash conditional expressions
+#   - -a arquivo existe
+#   - -d diretório existe
+#   - -f arquivo existe e é regular
+#   - -r arquivo existe e é legível
+#   - -x arquivo existe e é executável
+#   - file1 -nt -file2 arquivo 1 é mais novo que arquivo 2, ou se arquivo 1 existe e arquivo 2 não
+#   - file1 -ot -file2 arquivo 1 é mais velho que arquivo 2, ou se arquivo 2 existe e arquivo 1 função
+#   - -v variável variável tem um valor assiginado a declaradas
+#   - -z string verdadeiro se o tamanho da string for 0
+#   - -n string verdadeiro se o tamanho da string for diferente de 0
+
+
+
+
+}
+
+main
