a8c5cc09ff66a8a597a042040e82bcf0ecd03344
Jorge Souza da Silva <jorge.souza-da-silva@dxc.com>
2018-12-25 11:09:40 -0200

    if.sh modificado

--- if.sh
+++ if.sh
@@ -10,9 +10,71 @@ main() {
 #   - -v variável variável tem um valor assiginado a declaradas
 #   - -z string verdadeiro se o tamanho da string for 0
 #   - -n string verdadeiro se o tamanho da string for diferente de 0
+#   - ! inverte o valor booleano
 
+# Operadores de comparação aritméticos
+#   - arg1 -gt arg2 arg1 maior que arg2
+#   - arg1 -eq arg2 arg1 é igual a arg2
+#   - arg1 -lt arg2 arg1 é menor arg2
+#   - arg1 -ne arg2 arg1 não é igual a arg2
+#   - arg1 -ge arg2 arg1 é maior ou igual a arg2
+#   - arg1 -le arg2 arg1 é menor ou igual a arg2
+arq="arquivo-de-teste.txt"
+touch "$arq"
 
+arq2="arquivo-de-teste-2.txt"
+touch "$arq2"
+chmod 222 "$arq2"
 
+arquivo_existe "$arq"
+arquivo_existe "$arq2"
+arquivo_existe "naoexiste.txt"
+arquivo_existe "$0"
+
+dir="diretorio_de_teste"
+if [ ! -d "$dir" ]
+then
+  mkdir "$dir"
+fi
+
+diretorio_existe "$dir"
+
+if [ -v arq ]
+then
+  echo "Variável arq existe!"
+else
+  echo "Variável arq não existe!"
+fi
+
+rm -r "$dir"
+
+}
+
+diretorio_existe() {
+
+  if [ -d "$1" ]
+  then
+    echo "Diretório $1 existe!"
+  else
+    echo "Diretório $1 não existe!"
+  fi
+
+}
+
+arquivo_existe() {
+
+  if [ -x "$1" ]
+  then
+    echo "Arquivo $1 existe e é executável!"
+  elif [ -r "$1" ]
+  then
+    echo "Arquivo $1 existe e é legível!"
+  elif [ -a "$1" ]
+  then
+    echo "Arquivo existe, mas não é legível!"
+  else
+    echo "Arquivo não existe!"
+  fi
 
 }
 
