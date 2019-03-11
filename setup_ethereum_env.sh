18ec422d244fdca6213b85f2e6ff46c462c5a946
Jorge Souza da Silva <jorge.souza-da-silva@dxc.com>
2019-03-11 18:55:26 -0300

    Desenvolvimento setup_ethereum.sh

--- setup_ethereum_env.sh
+++ setup_ethereum_env.sh
@@ -3,9 +3,24 @@ JOB_NAME="setup_ethereum_env"
 USER_HOME="$HOME"
 
 # configuracao das portas
+NETWORK_ID=77
 PORT="30304"
 RPC_PORT="8546"
 CACHE=2048
+
+# pastas para instalacao
+DATA_DIR="${USER_HOME}/ethereum"
+
+# array de pastas
+FOLDERS=("$DATA_DIR")
+
+# pacotes que precisam ser instalados
+PACKAGES=("ethereum" "software-properties-common")
+REPOSITORIES=("ethereum")
+
+# configuracao da conta
+ACCOUNT=""
+DEFAULT_BALANCE=2000000
 
 write_log() {
 	
@@ -52,9 +67,7 @@ check_and_create_folder() {
 check_and_create_folders() {
 	write_log "[INFO] - Iniciando checagem e criacao de pastas."
 	
-	folders=("${USER_HOME}/ethereum-workspace")
-	
-	for folder in "${folders[@]}"
+	for folder in "${FOLDERS[@]}"
 	do
 		check_and_create_folder "$folder"
 	done
@@ -65,7 +78,7 @@ install_package() {
 
 	local package="$1"
 	
-	apt-get install "$package"
+	apt-get install -y "$package"
 	if [[  $? -eq 0 ]]
 	then
 		write_log "[INFO] - pacote $package instalado com sucesso."
@@ -95,38 +108,131 @@ add_apt_repository() {
 install_necessary_packages() {
 
 	write_log "[INFO] - iniciando instalacao dos pacotes necessarios."
-
-	repositories=("ethereum")
-	packages=("ethereum" "software-properties-common")
 	
-	for repository in "${repositories[@]}"
+	for repository in "${REPOSITORIES[@]}"
 	do
 		add_apt_repository "$repository"
 	done
 	
 	apt update
 	
-	for package in "${packages[@]}"
+	for package in "${PACKAGES[@]}"
 	do
 		install_package "$package"
 	done
 
 }
 
-create_ethereum_network() {
+start_ethereum_node() {
 
-	geth --rinkeby --datadir=~/.gophersland_ethereum_r1 \
-	--port="$PORT" --cache="$CACHE" --rpc --rpcport="$RPC_PORT" \
-	--rpcapi=eth,web3,net,personal --syncmode=fast
+	write_log "[INFO] - Criando rede ethereum."
+
+	geth --rinkeby \
+	--datadir="$DATA_DIR" \
+	--nodiscover console \
+	--unlock $ACCOUNT
+	--port="$PORT" --cache="$CACHE" \
+	--rpc --rpcport="$RPC_PORT" \
+	--networkid "$NETWORK_ID" \
+	--rpcapi=eth,web3,net,personal \
+	--syncmode=fast
+	
+	if [[  $? -eq 0  ]]
+	then
+		write_log "[INFO] - Rede ethereum criada com sucesso."
+		write_log "[INFO] - Porta: $PORT Network ID: $NETWORK_ID RPC PORT: $RPC_PORT"
+	else
+	fi
 
 }
 
+create_ethereum_new_account() {
+
+	write_log "[INFO] - Criando nova conta ethreum."
+	
+	local output=$(geth --datadir $DATA_DIR account new)
+	echo "$output" > "${JOB_NAME}.account"
+	if [[  $? -eq 0  ]]
+	then
+		write_log "[INFO] - Conta criada com sucesso."
+		ACCOUNT=$(echo "$output" | grep 'Address' | sed 's/.*{//' | sed 's/}//' )
+		if [[ -z $ACCOUNT ]]
+		then
+			write_log "[INFO] - Erro durante a captura da conta."
+			script_exit 1
+		else
+			write_log "[INFO] - Conta $ACCOUNT capturada com sucesso."
+		fi
+	else
+		write_log "[ERRO] - Erro durante a criacao da conta."
+		script_exit 1
+	fi
+
+}
+
+generate_genesis_json() {
+	
+	write_log "[INFO] - Iniciando geracao do arquivo genesis.json"
+
+	base_json='{
+		"config":{
+			"chainId":%s,
+			"homesteadBlock":0,
+			"eip155Block":0,
+			"eip158Block":0
+		},
+		"difficulty":"200000",
+		"gasLimit":"2100000000",
+		"alloc":{
+			"%s":{
+				"balance":"%s"
+			}
+		}
+	}'
+	
+	printf "$base_json" $NETWORK_ID $ACCOUNT $DEFAULT_BALANCE > "${DATA_DIR}/genesis.json"
+	if [[  $? -eq 0  ]]
+	then
+		write_log "[INFO] - Arquivo genesis.json gerado em ${DATA_DIR}/generate_genesis_json"
+	else
+		write_log "[INFO] - Erro durante a geracao do arquivo genesis.json"
+		script_exit 1
+	fi
+
+}
+
+setup_ethereum_network() {
+
+	write_log "[INFO] - Iniciando criacao da rede ethereum."
+	
+	geth --datadir=$DATA_DIR init "${DATA_DIR}/genesis.json"
+	if [[  $? -eq 0  ]]
+	then
+		write_log "[INFO]- Rede ethereum criada com sucesso."
+	else
+		write_log "[ERRO] - Erro durante a criacao da rede ethereum."
+		script_exit 1
+	fi
+
+}
 
 main() {
 
+	if [[  $EUID -ne 0 ]]
+	then
+		write_log "[ERRO] - Este escript precisa ser executado como root."
+		write_log "[INFO] - usagem: sudo ${JOB_NAME}.sh"
+		script_exit 1
+	fi
+	
+	write_log "[INFO] - Inicio da execucao dos script ${JOB_NAME}.sh."
+
 	check_and_create_folders
 	install_necessary_packages
-	create_ethereum_network
+	generate_genesis_json
+	create_ethereum_new_account
+	setup_ethereum_network
+	start_ethereum_node
 
 }
 
